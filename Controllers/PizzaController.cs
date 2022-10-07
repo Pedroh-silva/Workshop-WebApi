63aebad60ff21aaf6f551519fa669161e80bdc4d
Pedro Silva <pehenriquesilva1202@gmail.com>
2022-10-07 13:18:31 -0300

    HTTP verb GET

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -12,7 +12,22 @@ public class PizzaController : ControllerBase
     {
     }
 
-    // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll()
+    {
+        return PizzaService.GetAll();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
 
     // GET by Id action
 
